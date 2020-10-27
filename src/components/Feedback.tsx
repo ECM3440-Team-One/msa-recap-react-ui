a30e1f1078024e9492b763b8dd62cadba1fe6d92
Harper, Beth <bmh212@exeter.ac.uk>
2020-10-27 16:21:15 +0000

    add basic html for web form

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -9,9 +9,28 @@ function Feedback() {
             <h1 className="font-weight-light">Feedback</h1>
             <p>
               Please provide your feedback using the form below.
-            </p>
+              </p>
           </div>
         </div>
+        <form id="feedback-form" method="POST">
+            <div className="form-group">
+              <label>First name: *</label><br></br>
+              <input type="text" name="fname" className="input" placeholder="First Name" required/>
+              <br></br>
+              <label>Last name: *</label><br></br>
+              <input type="text" name="lname" className="input" placeholder="Last Name" required/>
+              <br></br>
+              <label>Email: *</label><br></br>
+              <input type="text" name="email" className="input" placeholder="Your Email Address" required/>
+            </div>
+            <div className="form-group">
+              <label>Your feedback: *</label><br></br>
+              <textarea className="textarea" name="message" placeholder="Comments" required></textarea>
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Submit" />
+            </div>
+          </form>
       </div>
     </div>
   );
