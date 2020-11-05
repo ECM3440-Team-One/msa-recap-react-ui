e2abe92115b225838b76e74c2f114f61c553e843
jennifer-platt <jcp215@exeter.ac.uk>
2020-11-05 22:51:02 +0000

    Starting again

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,7 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import CircularProgress from '@material-ui/core/CircularProgress';
+// import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles';
@@ -124,6 +124,10 @@ function Home() {
   */
 
   let enableRecording = () => {
+    projectName().then((name) => {
+      console.log(name);
+      setProject(name);
+    }).catch((err) => { console.warn(err) });
     enableMicrophone();
     enableScreenCap();
     setDisabled({ enable: true, start: false, pause: true, end: true, complete: true, upload: true, download: true, spinner: false });
@@ -138,7 +142,7 @@ function Home() {
   let pauseRecording = () => {
     pauseAudioCapture();
     pauseScreenCapture();
-    setDisabled({ enable: true, start: false, pause: true, end: false, complete: true, upload: true, download: true, spinner: false });
+    setDisabled({ enable: true, start: false, pause: true, end: false, complete: true, upload: true, download: true, spinner: false});
   }
 
   let endRecording = () => {
@@ -150,9 +154,7 @@ function Home() {
   }
 
   let upload = async () => {
-    projectName().then((name) => {
-      setProject(name);
-    }).catch((err) => { console.warn(err) });
+
     if (project) {
       console.info("Calling upload()");
       let ablob: Blob = getAudioCaptureBlob();
@@ -178,12 +180,12 @@ function Home() {
   }
   // Handles recording upload, getting blobs for playback and resetting Record section buttons 
   let completeRecording = () => {
-    setDisabled({ enable: false, start: true, pause: true, end: true, complete: true, upload: false, download: false, spinner: true });
+    // setDisabled({ enable: false, start: true, pause: true, end: true, complete: true, upload: false, download: false, spinner: true });
     upload();
-    new Promise(r => setTimeout(r, 2000)).then(() => {
-      getRecording(project);
-      setDisabled({ enable: false, start: true, pause: true, end: true, complete: true, upload: false, download: false, spinner: false });
-    });
+    // new Promise(r => setTimeout(r, 5000)).then(() => {
+    getRecording(project);
+    // setDisabled({ enable: false, start: true, pause: true, end: true, complete: true, upload: false, download: false, spinner: false });
+    // });
   }
 
   // Opens Upload panel after clicking Finished Editing button
@@ -225,8 +227,6 @@ function Home() {
               <Button variant="contained" color="primary" id="finishRecording" disabled={disabled.complete} onClick={completeRecording}>Finished Recording</Button>
             </div>
             <br />
-            {disabled.spinner && <CircularProgress />}
-            <br />
           </AccordionDetails>
         </Accordion>
 
@@ -254,7 +254,7 @@ function Home() {
           </AccordionSummary>
           <AccordionDetails>
             <div>
-              <TextField variant="outlined" required label="Name your recording" onChange={(evt) => { setFilename({ name: evt.target.value, disabled: false }) }} />
+              <TextField variant="outlined" required label="Name your recording" onChange={(evt) => { setFilename({name: evt.target.value, disabled: false}) }} />
             </div>
             <br />
             <div>
